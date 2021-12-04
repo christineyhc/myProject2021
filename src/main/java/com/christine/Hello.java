08a37c018f3f26fe5ecaf21115efc14d1842d5c5
Christine <christine59yhc@gmail.com>
2021-12-04 20:57:36 +0800

    Initial commit Learn JAVA :D

---
+++ src/main/java/com/christine/Hello.java
@@ -0,0 +1,9 @@
+package com.christine;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello JAVA~");
+        Person p=new Person();
+        p.Hello();
+    }
+}
---
+++ src/main/java/com/christine/Person.java
@@ -0,0 +1,7 @@
+package com.christine;
+
+public class Person {
+    public void Hello(){
+        System.out.println("Hello person~");
+    }
+}
